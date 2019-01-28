e025f2564080496d5aff381fe3b391042450dc3d
Karl Niebuhr <karl.booklover@gmail.com>
2019-01-28 16:39:48 -0300

    rebuild profiles to solve broken shit

--- routeros_night_queue.sh
+++ routeros_night_queue.sh
@@ -1,12 +1,13 @@
 # Paste this script in the console to generate night queues for existing pppoe based dynamic queues
 
+# delete all existing night profiles to clean up broken profiles
+:foreach i in=[/queue simple find name~"night"] do={/queue simple remove $i}
+
 :foreach i in=[/queue simple find] do={ \
     :local target [/queue simple get $i target]; \
     :local maxlimit [/queue simple get $i max-limit]; \
     :put ("adding " . $target . " " . $maxlimit); \
-    /queue simple add name="$target_night" target=$target max-limit=10M/10M priority=1/1 time=00h00m-06h00m,sun,mon,tue,wed,thu,fri,sat disabled=yes; \
+    /queue simple add name="$target_night" target=$target max-limit=0/0 priority=1/1 queue=pcq-upload-default/pcq-download-default time=00h00m-06h00m,sun,mon,tue,wed,thu,fri,sat disabled=yes; \
 }; \
 /queue simple move [find name~"night"] 0;
 
-# loop over night queues. Replace the print function with whatever you need.
-:foreach i in=[/queue simple find name~"night"] do={:local name [/queue simple get $i name]; :put $name}
